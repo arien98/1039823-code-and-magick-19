973d5740796e777142f837f6f5e9854ba0fed74e
Светлана Хамматова <sveta.wing@gmail.com>
2020-02-03 23:18:43 +0500

    Добавляет окно статистики

---
+++ js/stat.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_HEIGHT = 270;
+var CLOUD_WIDTH = 420;
+var SHADOW_MOVING = 10;
+var CLOUD_MARGIN = 20;
+var LINE_HEIGHT = 25;
+var BAR_MAX_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var barHeight = [];
+var renderCloud = function (ctx, color, x, y) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+var renderBar = function (ctx, color, x, y, h) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, BAR_WIDTH, h);
+};
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+};
+
+// eslint-disable-next-line no-unused-vars
+var renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, 'rgba(0, 0, 0, 0.7)', CLOUD_X + SHADOW_MOVING, CLOUD_Y + SHADOW_MOVING);
+  renderCloud(ctx, '#fff', CLOUD_X, CLOUD_Y);
+
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText = ('Ура вы победили!', CLOUD_X + CLOUD_MARGIN, CLOUD_Y + CLOUD_MARGIN);
+  ctx.fillText = ('Список результатов:', CLOUD_X + CLOUD_MARGIN, CLOUD_Y + CLOUD_MARGIN + LINE_HEIGHT);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < times.length; i++) {
+    barHeight[i] = times[i] / maxTime * BAR_MAX_HEIGHT;
+    if (i < 1) {
+      var barColor = 'rgba(255, 0, 0, 1)';
+    } else {
+      barColor = 'hsl(250, Math.random() * 100%, 50%)';
+    }
+    renderBar(ctx, barColor, (BAR_WIDTH + BAR_GAP) * i + CLOUD_MARGIN, CLOUD_MARGIN + LINE_HEIGHT * 2 + (BAR_MAX_HEIGHT - barHeight[i]), barHeight[i]);
+    ctx.fillText('names[i]', (BAR_WIDTH + BAR_GAP) * i + CLOUD_MARGIN, CLOUD_MARGIN + LINE_HEIGHT * 2 + BAR_MAX_HEIGHT + BAR_GAP / 2);
+  }
+};
