3f13be4b5856070fcccb344ab72d4b4649845c4f
Светлана Хамматова <sveta.wing@gmail.com>
2020-02-04 00:37:09 +0500

    Показывает такст облака

--- js/stat.js
+++ js/stat.js
@@ -11,6 +11,7 @@ var BAR_MAX_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var barHeight = [];
+var FONT = '16px PT Mono';
 var renderCloud = function (ctx, color, x, y) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -32,8 +33,9 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 'rgba(0, 0, 0, 0.7)', CLOUD_X + SHADOW_MOVING, CLOUD_Y + SHADOW_MOVING);
   renderCloud(ctx, '#fff', CLOUD_X, CLOUD_Y);
 
-  ctx.font = '18px PT Mono';
+  ctx.font = FONT;
   ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000';
   ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_MARGIN, CLOUD_Y + CLOUD_MARGIN);
   ctx.fillText('Список результатов:', CLOUD_X + CLOUD_MARGIN, CLOUD_Y + CLOUD_MARGIN + LINE_HEIGHT);
 
@@ -46,7 +48,8 @@ window.renderStatistics = function (ctx, names, times) {
     } else {
       barColor = 'hsl(250, Math.random() * 100%, 50%)';
     }
-    renderBar(ctx, barColor, (BAR_WIDTH + BAR_GAP) * i + CLOUD_MARGIN, CLOUD_MARGIN + LINE_HEIGHT * 2 + (BAR_MAX_HEIGHT - barHeight[i]), barHeight[i]);
-    ctx.fillText(names[i], (BAR_WIDTH + BAR_GAP) * i + CLOUD_MARGIN, CLOUD_MARGIN + LINE_HEIGHT * 2 + BAR_MAX_HEIGHT + BAR_GAP / 2);
+    renderBar(ctx, barColor, CLOUD_X + (BAR_WIDTH + BAR_GAP) * i + CLOUD_MARGIN, CLOUD_Y + CLOUD_MARGIN + LINE_HEIGHT * 2 + (BAR_MAX_HEIGHT - barHeight[i]), barHeight[i]);
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + (BAR_WIDTH + BAR_GAP) * i + CLOUD_MARGIN, CLOUD_Y + CLOUD_MARGIN + LINE_HEIGHT * 2 + BAR_MAX_HEIGHT + BAR_GAP / 2);
   }
 };
